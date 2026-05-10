4e22be1c5041fe6093e7414296a4b8eeb6a166b0
Daksh2224 <dakshrathi494@gmail.com>
2026-05-10 11:14:21 +0530

    Fix: Added CORS preflight handling to allow frontend-backend communication

--- backend/config.php
+++ backend/config.php
@@ -4,6 +4,11 @@ header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Authorization");
 header("Content-Type: application/json; charset=UTF-8");
 
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit();
+}
+
 $host = "localhost";
 $db_name = "student_mgmt_db";
 $username = "root";
